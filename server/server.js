1e537d207acbe7ac8f79566de06a0f0322e46302
SRIRAGHAVAN K <ayyangarbadrinarayanan@gmail.com>
2025-04-21 19:33:37 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -27,7 +27,7 @@ app.use(express.static(path.join(__dirname)))
 
 // Route to serve the index.html file
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"))
+  res.sendFile(path.join(__dirname, "./index.html"))
 })
 
 // Route to handle the contact form submission
